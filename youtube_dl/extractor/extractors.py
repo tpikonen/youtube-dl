f977793d16613a88e81ff449f12f750b5b06a024
Teemu Ikonen <tpikonen@gmail.com>
2019-04-11 18:20:30 +0300

    [radioplay] Add new extractor

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -915,6 +915,7 @@ from .radiode import RadioDeIE
 from .radiojavan import RadioJavanIE
 from .radiobremen import RadioBremenIE
 from .radiofrance import RadioFranceIE
+from .radioplay import RadioplayIE
 from .rai import (
     RaiPlayIE,
     RaiPlayLiveIE,
